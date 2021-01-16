bc6c24cbd4576fa43bced72e354a503f7be0a04d
Ignacio <dev.ignacio.h@gmail.com>
2021-01-15 23:06:50 -0300

    CREATE and SET Poster Pipe

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,3 +1,4 @@
+import { PipesModule } from './../pipes/pipes.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -22,7 +23,8 @@ import { RatingModule } from 'ng-starrating';
   imports: [
     CommonModule,
     RouterModule,
-    RatingModule
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
